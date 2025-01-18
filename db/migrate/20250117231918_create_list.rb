a34d2eebaea0ede590b74349ee19107f33a762a5
Lito Croy <146680987+litobot@users.noreply.github.com>
2025-01-18 11:14:07 -0700

    Initial Commit

---
+++ db/migrate/20250117231918_create_list.rb
@@ -0,0 +1,11 @@
+class CreateList < ActiveRecord::Migration[7.1]
+  def change
+    create_table :lists do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.references :location, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20250117231932_create_location.rb
@@ -0,0 +1,11 @@
+class CreateLocation < ActiveRecord::Migration[7.1]
+  def change
+    create_table :locations do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
